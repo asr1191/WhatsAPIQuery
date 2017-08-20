0e4c2c71840a3cb161badb859fa4d60395036f86
Amal S Raj <amalsraj@cet.ac.in>
2017-08-20 23:34:16 +0530

    Reached doorstep of WW with Puppeteer.. Going with Puppeteer

---
+++ initiate-puppeteer.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({args:['--no-sandbox']});
+  const page = await browser.newPage();
+  
+  await page.emulate({ viewport:{ 
+                                    width:1920,
+                                    height: 1080,
+                                    isLandscape: true
+                                },
+                        userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+                    });
+  await page.goto('https://web.whatsapp.com',{  waitUntil:'networkidle',
+                                                networkIdleTimeout:5000
+                                            });
+  await page.waitFor('.qrcode');
+  await page.waitFor(4000);
+
+  await page.screenshot({path: 'example.png'});
+
+  browser.close();
+})();
