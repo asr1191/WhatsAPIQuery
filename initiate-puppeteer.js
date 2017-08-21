865562cf2dee0c66768e0189e59c322e6fd4045e
Amal S Raj <amalsraj@cet.ac.in>
2017-08-21 12:44:16 +0530

    Cleanup, runs custom scripts, can't save queried image.

--- initiate-puppeteer.js
+++ initiate-puppeteer.js
@@ -1,24 +1,66 @@
 const puppeteer = require('puppeteer');
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length != 3){
+  console.log("Usage: node initiate-puppeteer.js <number>\n\nCountry code affixed to phone number is mandatory");
+  process.exit();
+}
+
 
 (async () => {
-  const browser = await puppeteer.launch({args:['--no-sandbox']});
+  const browser = await puppeteer.launch({
+    args:['--no-sandbox'],
+    headless: true
+  });
   const page = await browser.newPage();
   
-  await page.emulate({ viewport:{ 
-                                    width:1920,
-                                    height: 1080,
-                                    isLandscape: true
-                                },
-                        userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-                    });
-  await page.goto('https://web.whatsapp.com');
-  await page.waitFor('.qrcode');
-  await page.waitFor(7000);
+  await page.emulate({ 
+    viewport:{
+      width:1920,
+      height: 1080,
+      isLandscape: true
+    },
+  userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+  });
+
+  
+  await page.goto('https://web.whatsapp.com',{
+    timeout:45000
+  });
+  await page.waitFor('.icon-logo');
+  //await page.waitFor(10000);
 
   await page.screenshot({path: 'qrcode.png'});
   console.log('SCREENSHOT','shot saved to qrcode.png, use phone to authenticate.');
-  await page.waitFor(60000);
-  console.log('SCREENSHOT','waited 20 seconds, taking another shot, saved to deck.png');
+
+  await page.waitFor('.intro-image');
+  await page.waitFor(4000);
+
+  console.log('SCREENSHOT','taking another shot, saved to deck.png');
   await page.screenshot({path: 'deck.png'})
+
+  imgSRC = '';
+
+  page.on('console',(...args)=>{
+    console.log("[Inner Console]",args[0],args[1]);
+    if(args[0]=='QUERY'){
+      imgSRC = args[1];
+    }
+  });
+
+  await page.evaluate((nr)=>{
+    console.log("DEBUG","Inside evaluate()");
+    Store.ProfilePicThumb.find(nr + '@c.us').then((d)=>{console.log('QUERY',d.img);});
+  },process.argv[2]);
+
+  console.log("PROGRESS","waiting for 10 seconds")
+  await page.waitFor(10000);
+  
+  console.log("DEBUG", "imgSRC: " + imgSRC );
+  request(imgSRC).pipe(fs.createWriteStream( process.argv[2] + '.jpg'))
+
+  page.waitFor("5000")
+
   browser.close();
 })();
